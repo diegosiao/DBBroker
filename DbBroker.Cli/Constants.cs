b678139f66ff46f22a61c1bc7457c8a99a649d1d
Diego Morais <diegosiao@gmail.com>
2024-11-13 10:34:41 -0300

    v3.0.0 - rename dbbroker example project

--- DbBroker.Cli/Constants.cs
+++ DbBroker.Cli/Constants.cs
@@ -36,7 +36,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using DbBroker.Attributes;
 using DbBroker.Common;
-using DBBroker.Model;
+using DbBroker.Model;
 
 namespace $NAMESPACE;
 
