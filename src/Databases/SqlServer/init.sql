db525bf3be946c0649f1f6a5571021729e8924cc
DiegoDeMedeiros-JalaUniversity <diegosiao@gmail.com>
2025-10-06 10:48:24 -0300

    v.3.0.0 - Adding views (Oracle and SQL Server)

--- src/Databases/SqlServer/init.sql
+++ src/Databases/SqlServer/init.sql
@@ -6,6 +6,7 @@ GO
 USE [DBBroker]
 GO
 
+-- #TABLES
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -13,6 +14,7 @@ GO
 CREATE TABLE [dbo].[Customers](
 	[Id] [uniqueidentifier] NOT NULL,
 	[Name] [varchar](250) NOT NULL,
+	[Email] [varchar](250) NULL,
 	[Birthday] [date] NULL,
 	[OrdersTotal] [int] NULL,
 	[CreatedAt] [datetime] NOT NULL,
@@ -25,6 +27,21 @@ CREATE TABLE [dbo].[Customers](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
+CREATE TABLE [dbo].[Addresses](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Street] [varchar](250) NOT NULL,
+	[City] [varchar](100) NOT NULL,
+	[State] [varchar](50) NOT NULL,
+	[PostalCode] [varchar](20) NOT NULL,
+	[Country] [varchar](100) NOT NULL
+ CONSTRAINT [PK_Addresses] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -40,6 +57,7 @@ CREATE TABLE [dbo].[CustomersNotes](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -53,6 +71,7 @@ CREATE TABLE [dbo].[CustomersNotesStatus](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -60,6 +79,8 @@ GO
 CREATE TABLE [dbo].[Orders](
 	[Id] [uniqueidentifier] NOT NULL,
 	[CustomerId] [uniqueidentifier] NOT NULL,
+	[BillingAddressId] [uniqueidentifier] NOT NULL,
+	[ShippingAddressId] [uniqueidentifier] NOT NULL,
 	[StatusId] [int] NULL,
 	[CreatedAt] [datetime] NOT NULL,
 	[CreatedBy] [varchar](50) NOT NULL,
@@ -71,12 +92,14 @@ CREATE TABLE [dbo].[Orders](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[OrdersNotes](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[OrderId] [uniqueidentifier] NOT NULL,
 	[NoteContent] [varchar](1024) NOT NULL,
  CONSTRAINT [PK_OrderNotes] PRIMARY KEY CLUSTERED 
 (
@@ -84,6 +107,7 @@ CREATE TABLE [dbo].[OrdersNotes](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -98,19 +122,21 @@ CREATE TABLE [dbo].[OrdersProducts](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[OrderStatus](
+CREATE TABLE [dbo].[OrdersStatus](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Status] [varchar](50) NOT NULL,
- CONSTRAINT [PK_OrderStatus] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_OrdersStatus] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -118,12 +144,14 @@ GO
 CREATE TABLE [dbo].[Products](
 	[Id] [uniqueidentifier] NOT NULL,
 	[ProductName] [varchar](50) NOT NULL,
+	[Price] [decimal](18, 2) NOT NULL
  CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -138,6 +166,7 @@ CREATE TABLE [dbo].[Promotions](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -152,6 +181,33 @@ CREATE TABLE [dbo].[PromotionsEnrollments](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
+-- #CONSTRAINTS
+ALTER TABLE [dbo].[Customers] WITH CHECK ADD CONSTRAINT FK_CustomersAddressId FOREIGN KEY([Id])
+REFERENCES [dbo].[Addresses] ([Id])
+GO
+--
+ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_Customers] FOREIGN KEY([CustomerId])
+REFERENCES [dbo].[Customers] ([Id])
+GO
+ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_Customers]
+GO
+ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_OrdersStatus] FOREIGN KEY([StatusId])
+REFERENCES [dbo].[OrdersStatus] ([Id])
+GO
+ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_OrdersStatus]
+GO
+ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_BillingAddress] FOREIGN KEY([BillingAddressId])
+REFERENCES [dbo].[Addresses] ([Id])
+GO
+ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_BillingAddress]
+GO
+ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_ShippingAddress] FOREIGN KEY([ShippingAddressId])
+REFERENCES [dbo].[Addresses] ([Id])
+GO
+ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_ShippingAddress]
+GO
+--
 ALTER TABLE [dbo].[CustomersNotes]  WITH CHECK ADD  CONSTRAINT [FK_CustomerNotes_Customers] FOREIGN KEY([CustomerId])
 REFERENCES [dbo].[Customers] ([Id])
 GO
@@ -162,16 +218,13 @@ REFERENCES [dbo].[CustomersNotesStatus] ([Id])
 GO
 ALTER TABLE [dbo].[CustomersNotes] CHECK CONSTRAINT [FK_CustomersNotes_CustomersNotesStatus]
 GO
-ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_Customers] FOREIGN KEY([CustomerId])
-REFERENCES [dbo].[Customers] ([Id])
+--
+ALTER TABLE [dbo].[OrdersNotes]  WITH CHECK ADD  CONSTRAINT [FK_OrderNotes_Orders] FOREIGN KEY([OrderId])
+REFERENCES [dbo].[Orders] ([Id])
 GO
-ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_Customers]
+ALTER TABLE [dbo].[OrdersNotes] CHECK CONSTRAINT [FK_OrderNotes_Orders]
 GO
-ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_OrderStatus] FOREIGN KEY([StatusId])
-REFERENCES [dbo].[OrderStatus] ([Id])
-GO
-ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_OrderStatus]
-GO
+--
 ALTER TABLE [dbo].[OrdersProducts]  WITH CHECK ADD  CONSTRAINT [FK_OrderProducts_Orders] FOREIGN KEY([OrderId])
 REFERENCES [dbo].[Orders] ([Id])
 GO
@@ -182,6 +235,7 @@ REFERENCES [dbo].[Products] ([Id])
 GO
 ALTER TABLE [dbo].[OrdersProducts] CHECK CONSTRAINT [FK_OrderProducts_Products]
 GO
+--
 ALTER TABLE [dbo].[PromotionsEnrollments]  WITH CHECK ADD  CONSTRAINT [FK_PromotionsEnrollments_Customers] FOREIGN KEY([CustomerId])
 REFERENCES [dbo].[Customers] ([Id])
 GO
@@ -192,5 +246,33 @@ REFERENCES [dbo].[Promotions] ([Id])
 GO
 ALTER TABLE [dbo].[PromotionsEnrollments] CHECK CONSTRAINT [FK_PromotionsEnrollments_Promotions]
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Primary key' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'CustomersNotes', @level2type=N'COLUMN',@level2name=N'Id'
+
+
+-- #VIEWS
+CREATE OR ALTER VIEW VwOrderSummary
+AS
+SELECT 	c.Name AS CustomerName, 
+		o.StatusId AS OrderStatusId, 
+		s.Status AS OrderStatus, 
+		(SELECT COUNT(*) FROM OrdersProducts op WHERE op.OrderId = o.Id ) AS NumberOfItems
+FROM Orders o
+JOIN Customers c ON c.ID = o.CustomerId 
+JOIN OrdersStatus s ON s.Id = o.StatusId;
+GO
+
+CREATE OR ALTER VIEW VwOrders
+AS
+SELECT 
+		o.Id AS OrderId, 
+		o.CreatedAt AS OrderCreation,
+		o.CustomerId,
+		c.Name AS CustomerName,
+		p.Id AS ProductId,
+		p.ProductName,
+		p.Price
+FROM Orders o
+JOIN Customers c ON c.Id = o.CustomerId
+JOIN OrdersStatus s ON s.Id = o.StatusId
+JOIN OrdersProducts op ON op.OrderId = o.Id
+JOIN Products p ON p.Id = op.ProductId;
 GO
